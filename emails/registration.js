d88ee67fbbfa7d4a8ffba23ba75a291819a0c9ea
jellyfish12jellyfish <grinvichforum12@mail.ru>
2020-07-24 21:41:16 +0500

    + some improvements

--- emails/registration.js
+++ emails/registration.js
@@ -7,12 +7,26 @@ module.exports = function (email) {
         subject: 'Ваш аккаунт успешно создан!',
         html: `
             <style>
-            body {
-                background: #f0f0f0;
-            }
+                a{
+                padding: 10px;
+                border-radius: 5px;
+                background-color: #afafaf;
+                color: white;
+                }
+                .wrapper{
+                text-align: center;
+                max-width: 400px;
+                margin: 0 auto;
+                }
             </style>
+            <div class="wrapper">
             <h1>Добро пожаловать, ${email}</h1>
+            <p>Поздравляем с успешной регистрацией!</p>
+            <p>Если возникнут вопросы, то пишите смело нам!</p>
+            <hr>
+            <br>
             <a href="${keys.BASE_URL}/todo">Перейти на наш сайт</a>
+            </div>
         `
     }
 }
